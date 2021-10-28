bf501c8a1d5f21c021e26332985106e077386344
dxxlsxxm <dev.primesec@gmail.com>
2021-10-28 19:05:57 +0900

    add 211028

--- app/src/main/java/univ/ewha/chxckj/tdlkotan/MainActivity.kt
+++ app/src/main/java/univ/ewha/chxckj/tdlkotan/MainActivity.kt
@@ -8,4 +8,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    private var backPressed:Long = -1;
+    override fun onBackPressed() {
+        if (System.currentTimeMillis() - backPressed < 2000) {
+            finish()
+        } else {
+            backPressed = System.currentTimeMillis()
+        }
+    }
 }
---
+++ app/src/main/java/univ/ewha/chxckj/tdlkotan/fragment/MainFragment.java
@@ -0,0 +1,9 @@
+package univ.ewha.chxckj.tdlkotan.fragment;
+
+import android.view.View;
+
+import androidx.fragment.app.Fragment;
+
+public class MainFragment extends Fragment {
+
+}
